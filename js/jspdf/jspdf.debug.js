632c21140234e6c8f4da530014dca23123f65afc
Weihui Guo <weihui.guo@outlook.com>
2020-02-07 17:30:45 -0500

    moved zoom logic to html.js

--- js/jspdf/jspdf.debug.js
+++ js/jspdf/jspdf.debug.js
@@ -12216,6 +12216,7 @@
         var options = Object.assign({
           async: true,
           allowTaint: true,
+          scale: (this.prop.pageSize.width * this.prop.pageSize.k) / Math.max(this.prop.src.clientWidth, this.prop.src.scrollWidth, this.prop.src.offsetWidth),
           backgroundColor: '#ffffff',
           imageTimeout: 15000,
           logging: true,
